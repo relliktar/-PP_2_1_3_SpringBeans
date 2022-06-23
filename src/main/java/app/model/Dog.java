90411e3747309c6a9b3f03d236273dbe1125ae5a
Виталий Корнилов <ratlion@rambler.ru>
2022-06-23 13:30:48 +0300

    решение v1

---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,10 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog extends Animal{
+    public String toString(){
+        return "I`m a Dog!";
+    }
+}
